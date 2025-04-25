1b6b9ce450a0640f61c5c13fc2051e48a94f19c5
Lana <kainingyang22@gmail.com>
2025-04-25 12:23:38 +0800

    改後端連結

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ require("./config/passport")(passport);
 const cors = require("cors");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mongodbtext")
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("連接到MongoDB");
   })
